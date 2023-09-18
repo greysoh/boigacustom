cc877aaed166bad69559e8a9d0424c5605ff282f
greysoh <dev@hofers.cloud>
2023-09-18 15:35:31 -0400

    fix: Changes variables to work with .mjs.

--- lazyconvert.mjs
+++ lazyconvert.mjs
@@ -21,14 +21,14 @@ const Scratch = {
 const input = {}; //dont-remove-this
 
 if (typeof Deno != "undefined") {
-  const selfJS = await Deno.readTextFile("./lazyconvert.js");
+  const selfJS = await Deno.readTextFile("./lazyconvert.mjs");
 
   let selfJSRemovedInputReconstructed = selfJS.replace("const input = {", 0xDEADBEEF).split(0xDEADBEEF)[0];
   selfJSRemovedInputReconstructed += `const input = {}; //dont-remove-this`;
   selfJSRemovedInputReconstructed += selfJS.replace("const input = {", 0xDEADBEEF).split(0xDEADBEEF)[1].replace("//dont-remove-this", 0xDEADBEEF).split(0xDEADBEEF)[1];
 
   await Deno.writeTextFile("./pluginmeta.json", JSON.stringify(input, null, 2));
-  await Deno.writeTextFile("./lazyconvert.js", selfJSRemovedInputReconstructed);
+  await Deno.writeTextFile("./lazyconvert.mjs", selfJSRemovedInputReconstructed);
 } else {
   console.log(JSON.stringify(input, null, 2));
 }
